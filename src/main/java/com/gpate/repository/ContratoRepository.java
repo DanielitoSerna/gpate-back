9a8663847bfcb0494c8eca91498da5aa8aaa0197
danielitoserna <danielestebanse@gmail.com>
2023-07-19 18:56:12 -0500

    @CrossOrigin("*")

--- src/main/java/com/gpate/repository/ContratoRepository.java
+++ src/main/java/com/gpate/repository/ContratoRepository.java
@@ -2,10 +2,12 @@ package com.gpate.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.gpate.model.Contrato;
 
 @RepositoryRestResource(collectionResourceRel = "contratos", path = "contratos")
+@CrossOrigin("*")
 public interface ContratoRepository extends JpaRepository<Contrato, Integer> {
 
 }
