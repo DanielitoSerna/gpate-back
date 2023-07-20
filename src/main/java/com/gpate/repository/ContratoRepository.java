e1292ce78009c98b55b4dcb834cfa86bcbc672b0
danielitoserna <danielestebanse@gmail.com>
2023-07-19 22:05:03 -0500

    contratos

--- src/main/java/com/gpate/repository/ContratoRepository.java
+++ src/main/java/com/gpate/repository/ContratoRepository.java
@@ -1,13 +1,18 @@
 package com.gpate.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.gpate.model.Contrato;
+import com.querydsl.core.types.EntityPath;
+
 
 @RepositoryRestResource(collectionResourceRel = "contratos", path = "contratos")
 @CrossOrigin("*")
-public interface ContratoRepository extends JpaRepository<Contrato, Integer> {
+public interface ContratoRepository extends JpaRepository<Contrato, Integer>,
+	QuerydslPredicateExecutor<Contrato>{
 
 }
