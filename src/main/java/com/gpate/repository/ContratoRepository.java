15d679f4732de23ead59a3cc1755077c2f76359a
danielitoserna <danielestebanse@gmail.com>
2023-07-19 22:40:25 -0500

    Querydsl

--- src/main/java/com/gpate/repository/ContratoRepository.java
+++ src/main/java/com/gpate/repository/ContratoRepository.java
@@ -3,16 +3,25 @@ package com.gpate.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.querydsl.binding.SingleValueBinding;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.gpate.model.Contrato;
-import com.querydsl.core.types.EntityPath;
-
+import com.gpate.model.QContrato;
+import com.querydsl.core.types.dsl.StringExpression;
+import com.querydsl.core.types.dsl.StringPath;
 
 @RepositoryRestResource(collectionResourceRel = "contratos", path = "contratos")
 @CrossOrigin("*")
-public interface ContratoRepository extends JpaRepository<Contrato, Integer>,
-	QuerydslPredicateExecutor<Contrato>{
+public interface ContratoRepository extends JpaRepository<Contrato, Integer>, QuerydslPredicateExecutor<Contrato>,
+		QuerydslBinderCustomizer<QContrato> {
+
+	@Override
+	default void customize(QuerydslBindings bindings, QContrato root) {
+		bindings.bind(String.class)
+				.first((SingleValueBinding<StringPath, String>) StringExpression::containsIgnoreCase);
+	}
 
 }
