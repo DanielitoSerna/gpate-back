ee518d22fe4f6a1e41c68aaaef8b9c451905ce66
danielitoserna <danielestebanse@gmail.com>
2023-07-28 16:51:16 -0500

    config ids

--- src/main/java/com/gpate/GpateApplication.java
+++ src/main/java/com/gpate/GpateApplication.java
@@ -2,12 +2,22 @@ package com.gpate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import com.gpate.model.Contrato;
 
 @SpringBootApplication
-public class GpateApplication {
+public class GpateApplication implements RepositoryRestConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GpateApplication.class, args);
+	}
+	
+	@Override
+	public void configureRepositoryRestConfiguration(final RepositoryRestConfiguration config, final CorsRegistry cors) {
+		config.exposeIdsFor(Contrato.class);
 	}
 
 }
