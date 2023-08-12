370fa8265c9e035fb5de5261e3a3214fc778a4be
danielitoserna <danielestebanse@gmail.com>
2023-08-11 22:14:44 -0500

    ajustes diseño y fórmulas de días en contratos

--- src/main/java/com/gpate/validators/ContratoValidator.java
+++ src/main/java/com/gpate/validators/ContratoValidator.java
@@ -1,5 +1,7 @@
 package com.gpate.validators;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -18,6 +20,7 @@ public class ContratoValidator implements Validator {
 	public void validate(Object target, Errors errors) {
 		Contrato contrato = (Contrato) target;
 
+		// DIAS PROGRAMADOS
 		if (contrato.getFechaProgramadaEntrega() != null && contrato.getFechaSolicitudContrato() != null) {
 
 			int milisecondsByDay = 86400000;
@@ -27,6 +30,35 @@ public class ContratoValidator implements Validator {
 
 			contrato.setDiasProgramados(dias);
 		}
+
+		// DIAS DE ATENCION
+		if (contrato.getFechaFirmadoCliente() != null && contrato.getFechaFallo() != null) {
+			int milisecondsByDay = 86400000;
+
+			int dias = (int) ((contrato.getFechaFirmadoCliente().getTime() - contrato.getFechaFallo().getTime())
+					/ milisecondsByDay);
+
+			contrato.setDiasAtencion(dias);
+		}
+
+		// DIAS VENCIMIENTO
+		if (contrato.getFechaVencimientoContrato() != null) {
+			int milisecondsByDay = 86400000;
+
+			Date fechaActual = new Date();
+
+			Integer dias = (int) ((contrato.getFechaVencimientoContrato().getTime() - fechaActual.getTime())
+					/ milisecondsByDay);
+			if (dias < 0) {
+				contrato.setDiasVencimiento("CONTRATO VENCIDO ("+ dias.toString().replace("-", "")+" DIAS)");
+			} else {
+				contrato.setDiasVencimiento(dias.toString() + "DIAS PARA SU VENCIMIENTO");
+			}
+		} else {
+			contrato.setDiasVencimiento("FALTA FECHA DE VENCIMIENTO DE CONTRATO");
+		}
+
+		// STATUS GENERAL
 		if (contrato.getFechaFallo() != null) {
 			if (contrato.getFechaSolicitudContrato() != null) {
 				if (contrato.getFechaProgramadaEntrega() != null) {
