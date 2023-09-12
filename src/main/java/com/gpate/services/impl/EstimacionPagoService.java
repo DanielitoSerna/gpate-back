36ae4f772b5a6e620fae675f8fc0e576562d05da
danielitoserna <danielestebanse@gmail.com>
2023-09-12 16:54:03 -0500

    eliminar estimación y ajuste en carga masivo

--- src/main/java/com/gpate/services/impl/EstimacionPagoService.java
+++ src/main/java/com/gpate/services/impl/EstimacionPagoService.java
@@ -14,10 +14,10 @@ import com.gpate.services.IEstimacionPagoService;
 
 @Service
 public class EstimacionPagoService implements IEstimacionPagoService {
-	
+
 	@Autowired
 	private EstimacionPagoRepository estimacionPagoRepository;
-	
+
 	@Autowired
 	private ContratoRepository contratoRepository;
 
@@ -34,6 +34,15 @@ public class EstimacionPagoService implements IEstimacionPagoService {
 			if (estimacionPago != null) {
 				idContrato = estimacionPago.getContrato();
 				estimacionPagoRepository.delete(estimacionPago);
+				List<EstimacionPago> estimacionPagosAbonoEstimacion = estimacionPagoRepository
+						.findByNumeroAbonoAndContrato(estimacionPago.getNumeroAbono(), idContrato);
+				BigDecimal sumaAbonoEstimacion = new BigDecimal(0);
+				for (EstimacionPago estimacionPagoAbonoEstimacion : estimacionPagosAbonoEstimacion) {
+					if("ESTIMACIÓN".equals(concepto) && "ABONO A ESTIMACIÓN".equals(estimacionPagoAbonoEstimacion.getConcepto())) {
+						sumaAbonoEstimacion = sumaAbonoEstimacion.add(estimacionPagoAbonoEstimacion.getImporte());
+						estimacionPagoRepository.delete(estimacionPagoAbonoEstimacion);
+					}
+				}
 				Contrato contrato = contratoRepository.findById(idContrato).get();
 				if (contrato != null) {
 					BigDecimal estimacionProgramada = contrato.getEstimacionesProgramadas();
@@ -41,28 +50,52 @@ public class EstimacionPagoService implements IEstimacionPagoService {
 					BigDecimal pagoAplicado = contrato.getPagosAplicados();
 					BigDecimal saldoPendiente = contrato.getSaldoPendienteContrato();
 					BigDecimal montoContrato = contrato.getImporteContratado();
-					
+
 					List<EstimacionPago> estimacionPagos = estimacionPagoRepository.findByContrato(idContrato);
 					for (EstimacionPago estimacionPago2 : estimacionPagos) {
 						Integer numeroAbono2 = Integer.parseInt(estimacionPago2.getNumeroAbono());
 						if (estimacionPago2.getConcepto().equals(concepto)) {
 							if (numeroAbono < numeroAbono2) {
 								if (numeroAbono < 10) {
-									estimacionPago2.setNumeroAbono("0"+numeroAbono);
+									estimacionPago2.setNumeroAbono("0" + numeroAbono);
 								} else {
-									estimacionPago2.setNumeroAbono(numeroAbono+"");
+									estimacionPago2.setNumeroAbono(numeroAbono + "");
 								}
 								numeroAbono++;
 								estimacionPagoRepository.save(estimacionPago2);
 							}
 						}
 					}
-					
-					if("ESTIMACIÓN".equals(concepto)) {
+
+					if ("ESTIMACIÓN".equals(concepto)) {
 						estimacionProgramada = estimacionProgramada.subtract(importe);
+						estimcacionPagada = estimcacionPagada.subtract(sumaAbonoEstimacion);
+						pagoAplicado = pagoAplicado.subtract(sumaAbonoEstimacion);
+						saldoPendiente = montoContrato.subtract(pagoAplicado);
 						contrato.setEstimacionesProgramadas(estimacionProgramada);
+						contrato.setEstimacionesPagadas(estimcacionPagada);
+						contrato.setPagosAplicados(pagoAplicado);
+						contrato.setSaldoPendienteContrato(saldoPendiente);
 					}
-					if("ABONO A CONTRATO".equals(concepto)) {
+					if ("ABONO A ESTIMACIÓN".equals(concepto)) {
+						List<EstimacionPago> estimacionPagosEstimacion = estimacionPagoRepository
+								.findByNumeroAbonoAndContrato(estimacionPago.getNumeroAbono(), idContrato);
+						for (EstimacionPago estimacionPagoEstimacion : estimacionPagosEstimacion) {
+							if("ESTIMACIÓN".equals(estimacionPagoEstimacion.getConcepto())) {
+								BigDecimal importeEstimacion = estimacionPagoEstimacion.getImporteAbono();
+								importeEstimacion = importeEstimacion.subtract(importe);
+								estimacionPagoEstimacion.setImporteAbono(importeEstimacion);
+								estimacionPagoRepository.save(estimacionPagoEstimacion);
+							}
+						}
+						estimcacionPagada = estimcacionPagada.subtract(importe);
+						pagoAplicado = pagoAplicado.subtract(importe);
+						saldoPendiente = montoContrato.subtract(pagoAplicado);
+						contrato.setEstimacionesPagadas(estimcacionPagada);
+						contrato.setPagosAplicados(pagoAplicado);
+						contrato.setSaldoPendienteContrato(saldoPendiente);
+					}
+					if ("ABONO A CONTRATO".equals(concepto)) {
 						pagoAplicado = pagoAplicado.subtract(importe);
 						saldoPendiente = montoContrato.subtract(pagoAplicado);
 						contrato.setPagosAplicados(pagoAplicado);
@@ -74,15 +107,14 @@ public class EstimacionPagoService implements IEstimacionPagoService {
 						contrato.setPagosAplicados(pagoAplicado);
 						contrato.setSaldoPendienteContrato(saldoPendiente);
 					}
-					
 					contratoRepository.save(contrato);
 				}
 			}
-			
+			mensaje = "Ítem eliminado correctamente.";
 		} catch (Exception e) {
-			mensaje = "No encontrado.";
+			mensaje = "ïtem no encontrado.";
 		}
-		
+
 		return mensaje;
 	}
 
