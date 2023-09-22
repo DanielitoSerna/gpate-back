d63206d05acc2da12ed52b0bb933aa46fc1aa7eb
danielitoserna <danielestebanse@gmail.com>
2023-09-22 11:08:54 -0500

    ajuste en el cargue masivo de contrato

--- src/main/java/com/gpate/services/impl/ContratoService.java
+++ src/main/java/com/gpate/services/impl/ContratoService.java
@@ -150,7 +150,7 @@ public class ContratoService implements IContratoService {
 				}
 				String[] parts = line.split(";");
 				for (int i=0; i<parts.length; i++) {
-					parts[i] = parts[i].replace(" ", "");
+					parts[i] = parts[i].replace("-", "");
 				}
 				List<Contrato> contratos = new ArrayList<>();
 				count++;
